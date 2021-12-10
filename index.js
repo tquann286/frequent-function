8976d63d5cb34e6d22edfc9cfdbe41177af15f06
quantrung286 <88314050+tquann286@users.noreply.github.com>
2021-12-10 16:17:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,37 @@ const hexaColor = () => {
     }
     return hexa
 }
+
+// Get time 
+function showTime(){
+    let date = new Date()
+    let dateNow = date.getDate()
+    let year = date.getUTCFullYear()
+    let h = date.getHours() 
+    let m = date.getMinutes() 
+    let s = date.getSeconds() 
+    let session = "AM"
+    const months = [
+        "January", "February", 
+        "March", "April", "May", 
+        "June", "July", "August",
+        "September", "October", 
+        "November", "December"
+    ];
+
+    if(h == 0){
+        h = 12
+    }
+    
+    if(h > 12){
+        h = h - 12
+        session = "PM"
+    }
+    
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+    
+    let time = `${months[date.getMonth()]} ${dateNow}, ${year} ${h}:${m}:${s} ${session}`
+    document.getElementById("DigitalCLOCK").innerHTML = time
+}
