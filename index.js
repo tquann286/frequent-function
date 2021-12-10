986174be4d17489c9d1f9d59d00217badeb2a9a0
quantrung286 <88314050+tquann286@users.noreply.github.com>
2021-12-10 16:13:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// a function which generate random hexa colors
+const hexaColor = () => {
+    const str = '0123456789abcdef'
+    let hexa = '#'
+    let index
+    for (let i = 0; i < 6; i++) {
+      index = Math.floor(Math.random() * str.length)
+      hexa += str[index]
+    }
+    return hexa
+}
